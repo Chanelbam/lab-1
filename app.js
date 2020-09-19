0647a24d45cdd1c9b52594823c3da9df8e36dde5
Chanel Bam <chanelbam@Chanels-MacBook-Pro.local>
2020-09-19 14:46:19 -0700

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function converttocelsius (fahrenheit){
+    var celsius = (fahrenheit - 32 ) * 5 / 9
+console.log(celsius);
+var result = fahrenheit + "fahrenheit degree equals to" + celsius + "celsius degree"
+console.log(result);
+console.log(typeof(celsius))
+console.log(typeof(result))
+};
+converttocelsius(212)
