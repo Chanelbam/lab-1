3dc55a5042dd1c6185e7715e914912c29083286a
Chanel Bam <chanelbam@Chanels-MacBook-Pro.local>
2020-09-21 17:35:14 -0700

    Lab 1 Completed

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ console.log(typeof(celsius))
 console.log(typeof(result))
 };
 converttocelsius(212)
+
+
+
+
+var FavouriteMovies = [];
+
+function addFavouriteMovies (movieName){
+    if (movieName.includes("The") == false){
+        FavouriteMovies.push (movieName);
+    }
+}
+addFavouriteMovies("The Titanic")
+addFavouriteMovies("Spiderman")
+addFavouriteMovies("Spirtied Away")
+addFavouriteMovies("Totoro")
+addFavouriteMovies("Firefly")
+console.log(FavouriteMovies)
+
+var NumberOfMovies = FavouriteMovies.length
+function printFavouriteMovie(){
+    console.log("Favourite Movies..:" + NumberOfMovies)
+    for(var i = 0; i < NumberOfMovies; i ++){
+        console.log(i)
+        console.log(FavouriteMovies[i])
+    }
+}
+
+printFavouriteMovie()
+
